f5627e5d15757176076ee5ee7363aaef0d4934a8
Francesco Agnoletti <francesco.agnoletti3@studio.unibo.it>
2018-10-18 18:23:22 +0200

    Added SignUp and SignIn layout. Started SignUp behavior.

--- app/src/main/java/com/example/agnohendrix/androidonlinequizapp/MainActivity.java
+++ app/src/main/java/com/example/agnohendrix/androidonlinequizapp/MainActivity.java
@@ -1,13 +1,71 @@
 package com.example.agnohendrix.androidonlinequizapp;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.rengwuxian.materialedittext.MaterialEditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    MaterialEditText editNewUserName, editNewPassword, editNewEmail; //For Sign Up
+
+    MaterialEditText editUserName, editPassword; //For Sign In
+
+    Button btnSignUp, btnSignIn;
+
+    FirebaseDatabase database;
+    DatabaseReference users;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        database = FirebaseDatabase.getInstance();
+        users = database.getReference("Users");
+
+        editUserName = findViewById(R.id.editUserName);
+        editPassword = findViewById(R.id.editPassword);
+
+        btnSignIn = findViewById(R.id.btn_sign_in);
+        btnSignUp = findViewById(R.id.btn_sign_up);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showSignupDialog();
+            }
+        });
+    }
+
+    private void showSignupDialog() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
+        alertDialog.setTitle("Sign Up");
+        alertDialog.setMessage("Please fill full information");
+
+        LayoutInflater inflater = this.getLayoutInflater();
+        View sign_up_layout = inflater.inflate(R.layout.sign_up_layout, null);
+
+        editNewUserName = sign_up_layout.findViewById(R.id.editNewUserName);
+        editNewPassword = sign_up_layout.findViewById(R.id.editNewPassword);
+        editNewEmail = sign_up_layout.findViewById(R.id.editNewEmail);
+
+        alertDialog.setView(sign_up_layout);
+        alertDialog.setIcon(R.drawable.ic_account_circle_black_24dp);
+
+        alertDialog.setNegativeButton("NO", new DialogInterface.OnClickListener(){
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i){
+                dialogInterface.dismiss();
+            }
+        });
+        
     }
 }
