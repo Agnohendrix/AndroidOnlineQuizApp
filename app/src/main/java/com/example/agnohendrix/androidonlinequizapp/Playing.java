4d444e191405023e9e07b7504f2bccf57788e8de
Francesco Agnoletti <francesco.agnoletti3@studio.unibo.it>
2018-11-03 18:14:28 +0100

    Added answer Listeners.

--- app/src/main/java/com/example/agnohendrix/androidonlinequizapp/Playing.java
+++ app/src/main/java/com/example/agnohendrix/androidonlinequizapp/Playing.java
@@ -1,15 +1,20 @@
 package com.example.agnohendrix.androidonlinequizapp;
 
+import android.content.Intent;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import com.example.agnohendrix.androidonlinequizapp.Common.Common;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class Playing extends AppCompatActivity {
+public class Playing extends AppCompatActivity implements View.OnClickListener{
 
     final static long INTERVAL = 1000;
     final static long TIMEOUT = 7000;
@@ -25,11 +30,55 @@ public class Playing extends AppCompatActivity {
 
     ProgressBar progressBar;
     ImageView question_image;
-    
+    Button btnA, btnB, btnC, btnD;
+    TextView txtScore, txtQuestionNum, question_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_playing);
+
+        //Firebase
+        database = FirebaseDatabase.getInstance();
+        questions = database.getReference("Questions");
+
+        //View
+        txtScore =(TextView)findViewById(R.id.txtScore);
+        txtQuestionNum = (TextView)findViewById(R.id.txtTotalQuestions);
+        question_text = (TextView)findViewById(R.id.question_text);
+
+        progressBar = (ProgressBar)findViewById(R.id.progressBar);
+
+        btnA = (Button) findViewById(R.id.btnAnswerA);
+        btnB = (Button) findViewById(R.id.btnAnswerB);
+        btnC = (Button) findViewById(R.id.btnAnswerC);
+        btnD = (Button) findViewById(R.id.btnAnswerD);
+
+        btnA.setOnClickListener(this);
+        btnB.setOnClickListener(this);
+        btnC.setOnClickListener(this);
+        btnD.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        mCountDown.cancel();
+        //More questions
+        if(index < totalQuestion){
+            Button clickedButton = (Button)v;
+            //Correct answer
+            if(clickedButton.getText().equals(Common.questionList.get(index).getCorrectAnswer())){
+                score+=10;
+                correctAnswer++;
+                showQuestion(++index);
+            } else {
+                Intent intent = new Intent(this, Done.class);
+            }
+        }
+    }
+
+    private void showQuestion(int i) {
     }
 }
