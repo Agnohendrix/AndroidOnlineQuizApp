c231f3a1fb4b415cd07e72629f670f0a52210625
Francesco Agnoletti <francesco.agnoletti3@studio.unibo.it>
2018-11-06 18:19:18 +0100

    Ranking working, creating Ranking View

--- app/src/main/java/com/example/agnohendrix/androidonlinequizapp/RankingFragment.java
+++ app/src/main/java/com/example/agnohendrix/androidonlinequizapp/RankingFragment.java
@@ -6,14 +6,29 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.agnohendrix.androidonlinequizapp.Common.Common;
+import com.example.agnohendrix.androidonlinequizapp.Interface.RankingCallback;
+import com.example.agnohendrix.androidonlinequizapp.Model.QuestionScore;
+import com.example.agnohendrix.androidonlinequizapp.Model.Ranking;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class RankingFragment extends Fragment {
     View myFragment;
+
+    FirebaseDatabase database;
+    DatabaseReference questionScore, rankingtbl;
+
+    int sum=0;
 
     public static RankingFragment newInstance(){
         RankingFragment rankingFragment = new RankingFragment();
@@ -23,6 +38,10 @@ public class RankingFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        database = FirebaseDatabase.getInstance();
+        questionScore = database.getReference("Question_Score");
+        rankingtbl = database.getReference("Ranking");
     }
 
     @Nullable
@@ -30,6 +49,54 @@ public class RankingFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         myFragment = inflater.inflate(R.layout.fragment_ranking, container, false);
 
+        updateRanking(Common.currentUser.getUserName(), new RankingCallback<Ranking>() {
+            @Override
+            public void callBack(Ranking ranking) {
+                rankingtbl.child(ranking.getUserName()).setValue(ranking);
+                //showRanking();
+            }
+        });
+
         return myFragment;
+    }
+
+    private void showRanking() {
+
+        rankingtbl.orderByChild("score")
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        for(DataSnapshot data: dataSnapshot.getChildren()){
+                            Ranking local = data.getValue(Ranking.class);
+                            Log.d("DEBUG", local.getUserName());
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+    }
+
+    private void updateRanking(final String userName, final RankingCallback<Ranking> callback) {
+        questionScore.orderByChild("user").equalTo(userName)
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        for(DataSnapshot data: dataSnapshot.getChildren()){
+                            QuestionScore ques = data.getValue(QuestionScore.class);
+                            sum+=Integer.parseInt(ques.getScore());
+                        }
+                        Ranking ranking = new Ranking(userName, sum);
+                        callback.callBack(ranking);
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
     }
 }
